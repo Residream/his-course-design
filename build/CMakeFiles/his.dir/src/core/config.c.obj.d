a35bc46e24f70af6a8b2c320122662a32152160f
Residream <2123103812@qq.com>
2026-04-07 21:38:02 +0800

    remove build directory from repo

--- build/CMakeFiles/his.dir/src/core/config.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/his.dir/src/core/config.c.obj: \
- C:\Users\21231\Desktop\VScode\项目\HIS\src\core\config.c \
- C:/Users/21231/Desktop/VScode/项目/HIS/include/core/config.h
--- build/CMakeFiles/his.dir/src/core/main.c.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/his.dir/src/core/main.c.obj: \
- C:\Users\21231\Desktop\VScode\项目\HIS\src\core\main.c \
- C:/Users/21231/Desktop/VScode/项目/HIS/include/ui/menu.h \
- C:/Users/21231/Desktop/VScode/项目/HIS/include/core/config.h \
- C:/Users/21231/Desktop/VScode/项目/HIS/include/core/session.h
